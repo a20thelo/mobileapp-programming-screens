94684a8bd4ce1cf4d24b0d4d624e415d3110f1e0
there <a20thelo@student.his.se>
2021-04-22 09:22:26 +0200

    Added reference to button buttonmain (findviewbyid) and added intent

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -2,7 +2,11 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button button = findViewById(R.id.buttonmain);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                Log.d("TAG", "Hello");
+
+
+                finish();
+
+            }
+        });
+
     }
 }
